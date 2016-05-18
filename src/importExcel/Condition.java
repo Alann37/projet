9a155827c99c9a4b30ce16172d65417bcc43e6a4
Alann37 <damien.binet@pas-ici.net>
2016-05-18 16:07:23 +0200

    mise en place
    mise en place des sources

---
+++ src/importExcel/Condition.java
@@ -0,0 +1,177 @@
+package importExcel;
+
+public class Condition {
+	// 0 superieur 1 inferieur 2 egaliter 3 difference 4 checkbox 5 minmax 6 date 
+	int type;
+	boolean skip;
+	boolean isCheckBox;
+	String questionSkip;
+	double min;
+	double max;
+	double inf;
+	double sup;
+	double eq;
+	double neq;
+	int[] checkbox;
+	String tag;
+	
+	public Condition(String condition){
+		String newCondition = "";
+			if(condition.contains("_")){
+				if(condition.split("_").length==2){
+					tag = "_"+condition.split("_")[1];
+					condition = condition.replaceAll(tag,"");
+				}
+				if(condition.split("_").length==3){
+					tag = "_"+condition.split("_")[1];
+					tag+="_"+condition.split("_")[2];
+					condition = condition.replaceAll(tag,"");
+					}
+	
+			}else{
+				tag = null;
+			}
+			newCondition = "";
+			if(condition.contains("->")){
+				String preGoTo = condition.split("->")[0];
+				questionSkip = condition.split("->")[1];
+				skip = true;
+				if(condition.contains("<")){
+					newCondition = preGoTo.replaceAll("[^\\d.]", "");
+					sup = Integer.parseInt(newCondition);
+					type=0;
+				}else if(condition.contains(">")){
+					newCondition = preGoTo.replaceAll("[^\\d.]", "");
+					inf = Integer.parseInt(newCondition);
+					type=1;
+				}else if(condition.contains("==")){
+					type = 2;
+					newCondition = preGoTo.replaceAll("[^\\d.]", "");
+					newCondition = newCondition.replaceAll("==","");
+					eq = Double.valueOf(newCondition);
+				} else if(condition.contains("=/=")){
+					type = 3;
+					newCondition = preGoTo.replaceAll("[^\\d.]", "");
+					newCondition = newCondition.replaceAll("=/=","");
+					neq = Double.valueOf(newCondition);
+				} else if(condition.contains(",")){
+					type = 4;
+					String [] possibility = preGoTo.split(",");
+					checkbox = new int[possibility.length];
+					for(int i = 0 ; i < possibility.length; i++){
+						checkbox[i] = Integer.valueOf(possibility[i]);					
+					}
+				}else if(condition.contains("date")){
+					type = 6;
+					newCondition = preGoTo.replaceAll("date","");
+					newCondition = newCondition.replaceAll("MIN","");
+					newCondition= newCondition.replaceAll("MAX","");
+					newCondition = newCondition.replaceAll(" ","");
+					min = Integer.parseInt(newCondition.split("-")[0]);
+					newCondition = newCondition.split("-")[1];
+					newCondition = newCondition.replaceAll("[^\\d.]", "");
+					max = Integer.parseInt(newCondition);
+					tag = "date";
+				}else if(condition.contains("MIN") && !condition.contains("date")){
+					type = 5;
+					newCondition = preGoTo.replaceAll("MIN","");
+					newCondition= newCondition.replaceAll("MAX","");
+					newCondition = newCondition.replaceAll(" ","");
+					min = Integer.parseInt(newCondition.split("-")[0]);
+					newCondition = newCondition.split("-")[1];
+					newCondition = newCondition.replaceAll("[^\\d.]", "");
+					max = Integer.parseInt(newCondition);
+				}
+			} else {
+				if(condition.contains("<")){
+					newCondition = condition.replaceAll("[^\\d.]", "");
+					sup = Integer.parseInt(newCondition);
+					type=0;
+				}else if(condition.contains(">")){
+					newCondition = condition.replaceAll("[^\\d.]", "");
+					inf = Integer.parseInt(newCondition);
+					type=1;
+				}else if(condition.contains("==")){
+					type = 2;
+					newCondition = condition.replaceAll("[^\\d.]", "");
+					newCondition = newCondition.replaceAll("==","");
+					eq = Double.valueOf(newCondition);
+				} else if(condition.contains("=/=")){
+					type = 3;
+					newCondition = condition.replaceAll("[^\\d.]", "");
+					newCondition = newCondition.replaceAll("=/=","");
+					neq = Double.valueOf(newCondition);
+				} else if(condition.contains(",")){
+					type = 4;
+					String [] possibility = condition.split(",");
+					checkbox = new int[possibility.length];
+					for(int i = 0 ; i < possibility.length; i++){
+						checkbox[i] = Integer.valueOf(possibility[i]);					
+					}
+				}else if(condition.contains("date")){
+					type = 6;
+					newCondition = condition.replaceAll("date","");
+					newCondition = newCondition.replaceAll("MIN","");
+					newCondition= newCondition.replaceAll("MAX","");
+					newCondition = newCondition.replaceAll(" ","");
+					min = Integer.parseInt(newCondition.split("-")[0]);
+					newCondition = newCondition.split("-")[1];
+					newCondition = newCondition.replaceAll("[^\\d.]", "");
+					max = Integer.parseInt(newCondition);
+					tag = "date";
+				}else if(condition.contains("MIN") && !condition.contains("date")){
+					type = 5;
+					newCondition = condition.replaceAll("MIN","");
+					newCondition= newCondition.replaceAll("MAX","");
+					newCondition = newCondition.replaceAll(" ","");
+					min = Integer.parseInt(newCondition.split("-")[0]);
+					newCondition = newCondition.split("-")[1];
+					newCondition = newCondition.replaceAll("[^\\d.]", "");
+					max = Integer.parseInt(newCondition);
+				}
+				
+			}	
+		}
+	
+
+	@Override
+	public String toString() {
+		String sRet = "";
+		if(type == 0){
+			sRet = sup +" < ";
+			if(tag !=null){
+				sRet += " " + tag;
+			}
+		}
+		if(type == 1){
+			sRet = inf + " >";
+			if(tag !=null){
+				sRet += " " + tag;
+			}
+		}
+		if (type == 2){
+			sRet =  "==" +eq;
+			if(tag !=null){
+				sRet += " " + tag;
+			}
+		} 
+		if (type ==3){
+			sRet =  "=/=" + neq ;
+			if(tag !=null){
+				sRet += " " + tag;
+			}
+		}
+		if (type == 5 || type ==6){
+			sRet = "min : " + min+ " max : "+ + max;
+			if(tag !=null){
+				sRet += " " + tag;
+			}
+		}
+		return sRet;
+	}
+
+	public void setCondition(String condition,boolean skip , String questionSkip){
+		
+	}
+
+}
---
+++ src/importExcel/TraitementEtude.java
@@ -0,0 +1,99 @@
+package importExcel;
+
+import java.util.List;
+
+public class TraitementEtude {
+	private String etudeName;
+	private List<Question> questions;
+	private List<TraitementEntrer> etudes;
+	public String getEtudeName() {
+		return etudeName;
+	}
+	public void setEtudeName(String etudeName) {
+		this.etudeName = etudeName;
+	}
+	public List<Question> getQuestion() {
+		return questions;
+	}
+	public void setQuestion(List<Question> question) {
+		this.questions = question;
+	}
+	@Override
+	public String toString() {
+		String sRet = etudeName;
+		for(int i = 0 ; i < questions.size(); i ++){
+			sRet+="\n";
+			sRet+=questions.get(i).name;
+			for(int j = 0 ; j < questions.get(i).conditions.size();j++){
+				sRet+="\n";
+				sRet+="   " + questions.get(i).conditions.get(j);
+				
+			}
+		}
+		return sRet;
+	}
+	public TraitementEtude(String etudeName, List<Question> question) {
+		this.etudeName = etudeName;
+		this.questions = question;
+	}
+	public List<TraitementEntrer> getEtudes() {
+		return etudes;
+	}
+	public void setEtudes(List<TraitementEntrer> etudes) {
+		this.etudes = etudes;
+	}
+	public void checkEtude(){
+		/*int[] success = new int[etudes.size()];
+		int numberFail = 0;
+		int unfail = 0;
+		*/
+		for(int i = 0 ; i < etudes.size(); i ++){
+			TraitementEntrer temp = etudes.get(i);
+			temp.setQuestionName(questions);
+			
+			for(int j = 0 ; j < temp.getReponses().size();j++){
+				Reponse rTemp = temp.getReponses().get(j);
+				if(rTemp.questionName!=null){
+					for(int h = 0 ; h < questions.size() ; h ++){
+						if(questions.get(h).name.equals(rTemp.questionName)){
+							questions.get(h).reponses.add(rTemp);
+							//h=questions.size();
+						}
+					}
+				}
+			}
+			QuestionReturn skipTo= new QuestionReturn(true,false,"",-1) ;
+			QuestionReturn previousSkipTo;
+			for(int t = 0 ; t < questions.size(); t++){
+				//System.out.println("originalQuestion "+ questions.get(t).name + " with replace "+questions.get(t).questionNumber);
+			//	QuestionReturn returnQuest = questions.get(t).applyCondition(skipTo);
+				if(!questions.get(t).applyCondition()){
+					temp.setDisqualif(true);
+					//numberFail++;
+					temp.setQuestionDisqualif(questions.get(t).name);
+					t =questions.size();
+				} 
+			
+			}
+			for(int o = 0 ; o <questions.size() ; o ++){
+				questions.get(o).reponses.clear();
+			}
+			this.etudes.set(i, temp);
+		}
+	/*	for(int i = 0; i<etudes.size();i++){
+			if(!etudes.get(i).isDisqualif()){
+				success[unfail]=i;
+				unfail++;
+			}
+		}
+		System.out.println("Number of disqualif : "+numberFail +" on " + etudes.size() + " study");
+		for(int i = 0 ; i < unfail; i++){
+			//System.out.println("complete study is number : " + success[i]);
+		}*/
+		
+	}
+	public TraitementEtude(){
+		
+	}
+	
+}
