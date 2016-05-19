14b99200a6aa4976f1b5f19fcf3cce246e6ef12f
toleen <toleen@df998981-6338-f3ee-55df-ce57b7db7bff>
2016-05-19 14:01:57 +0000

    constSum LoopLess
    git-svn-id: https://github.com/Alann37/projet.git/trunk@17 df998981-6338-f3ee-55df-ce57b7db7bff
    
    
    git-svn-id: https://github.com/Alann37/projet.git/trunk@49 df998981-6338-f3ee-55df-ce57b7db7bff
    
    
    git-svn-id: https://github.com/Alann37/projet.git/trunk@86 df998981-6338-f3ee-55df-ce57b7db7bff

--- src/importExcel/skipCondition.java
+++ src/importExcel/skipCondition.java
@@ -3,7 +3,7 @@ package importExcel;
 public class skipCondition {
 	String loopNumber;
 	String questionName;
-	public skipCondition( String questionName, String loopNumber) {
+	public skipCondition(String loopNumber, String questionName) {
 	
 		this.loopNumber = loopNumber;
 		this.questionName = questionName;
